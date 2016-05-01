00978ee61d2fe6bf59eac723eb7cf86cc7579e57
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-05-01 15:28:16 +0100

    add package information to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule Deltek.Mixfile do
   use Mix.Project
+  @version "0.0.1"
 
   def project do
     [app: :deltek,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.2",
+     description: "An Elixir wrapper for the SOAP Deltek API",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -15,6 +18,13 @@ defmodule Deltek.Mixfile do
       applications: [:logger, :sweet_xml],
       mod: {Deltek, []}
     ]
+  end
+
+  defp package do
+    [maintainers: ["Andrea Rossi"],
+     files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/lucidstack/elixir-deltek"}]
   end
 
   defp deps do
